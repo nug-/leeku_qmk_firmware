bf74620ebfb79f30a9e1ea8c2c197c26881c7a60
nug- <nug_@outlook.com>
2020-07-15 12:26:29 +1000

    Updated config.h

--- keyboards/l3_finger65/keymaps/nug/config.h
+++ keyboards/l3_finger65/keymaps/nug/config.h
@@ -4,6 +4,9 @@
 #include "../../config.h"
 
 // place overrides here
+#undef TAPPING_TERM
+#undef TAPPING_TOGGLE
+#undef RGBLIGHT_SLEEP
 #define TAPPING_TOGGLE 2
 #define TAPPING_TERM 175
 #define RGBLIGHT_SLEEP

dab14cfe2c55cbe9437b920ed3b990f089db0182
nug- <nug_@outlook.com>
2020-07-16 13:19:40 +1000

    Updated keymap

--- keyboards/l3_finger65/keymaps/nug/keymap.c
+++ keyboards/l3_finger65/keymaps/nug/keymap.c
@@ -2,6 +2,7 @@
 #include QMK_KEYBOARD_H
 
 #define LT_L1SP LT(_L1, KC_SPC) /* SPACE on tap, LAYER ONE on hold */
+#define LT_L1DE LT(_L1, KC_DEL) /* DEL on tap, LAYER ONE on hold */
 
 enum layers {
   _L0 = 0,
@@ -19,7 +20,6 @@ enum {
   SINGLE_TAP = 1,
   SINGLE_HOLD,
   DOUBLE_TAP,
-  TRIPLE_TAP,
 };
 
 //Tap dance enums
@@ -52,14 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, KC_HOME,
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
     TD(TCAP), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,           KC_PGDN,
-    KC_LSFT,  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,
+    KC_LSFT,  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   LT_L1DE,
     KC_LCTL,  KC_LGUI, KC_LALT, KC_NO,                     LT_L1SP, KC_NO,   KC_RALT, KC_RCTL,                            KC_LEFT, KC_DOWN, KC_RGHT),
   
   [_L1] = KEYMAP(
     KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_DEL,  KC_DEL,
     _______,  _______, KC_UP,   _______, _______, _______, _______, _______, KC_PGUP, _______, _______, _______, _______, KC_INS,           KC_INS,
     _______,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_HOME, KC_PGDN, KC_END,  _______, _______, KC_NO,   KC_PSCR,          KC_PSCR,
-    _______,  KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, KC_DEL,
+    _______,  KC_NO,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP, _______,
     _______,  _______, _______, KC_NO,                     _______, KC_NO,   _______, _______,                            KC_HOME, KC_PGDN, KC_END),
 
   [_L2] = KEYMAP(
@@ -68,13 +68,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,           KC_PGDN,
     KC_LSFT,  KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,
     KC_LCTL,  KC_NO,   KC_LALT, KC_NO,                     KC_SPC,  KC_NO,   KC_RALT, KC_RCTL,                            KC_LEFT, KC_DOWN, KC_RGHT),
-
-  [_L3] = KEYMAP(
-    RESET,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,
-    _______,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,
-    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,
-    KC_NO,    KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_NO,   KC_NO,   KC_NO),
 };
 
 int cur_dance (qk_tap_dance_state_t *state) {
@@ -85,10 +78,7 @@ int cur_dance (qk_tap_dance_state_t *state) {
   else if (state->count == 2) {
     return DOUBLE_TAP;
   }
-  else if (state->count == 3) {
-    return TRIPLE_TAP;
-  }
-  else return 5; //magic number. At some point this method will expand to work for more presses
+  else return 4; //magic number. At some point this method will expand to work for more presses
 }
 
 //instanalize an instance of 'tap' for the 'x' tap dance.
@@ -109,9 +99,6 @@ void x_finished (qk_tap_dance_state_t *state, void *user_data) {
     case DOUBLE_TAP: /*toggle gaming layer*/
       layer_invert(2); 
       break;
-    case TRIPLE_TAP: /*toggle reset layer*/
-      layer_invert(3); 
-      break;
   }
 }
 
@@ -124,8 +111,6 @@ void x_reset (qk_tap_dance_state_t *state, void *user_data) {
       layer_off(1); 
       break;
     case DOUBLE_TAP: 
-      break;
-    case TRIPLE_TAP: 
       break;
   }
   xtap_state.state = 0;
